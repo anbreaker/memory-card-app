733d133e3b53c99124925796935e024ec9edad15
Francisco Javier Antúnez Durán <javier.antunez@sngular.com>
2025-01-14 10:04:11 +0100

    feat: add JSDoc comments to CountdownTimer component for better documentation

--- src/components/countdown-timer/countdown-timer.js
+++ src/components/countdown-timer/countdown-timer.js
@@ -11,10 +11,18 @@ import { ONE_SECOND } from '../../constants/game-constants.js';
 import countdownTimerStyles from './countdown-timer.css.js';
 
 export class CountdownTimer extends i18nMixin(LitElement) {
+  /**
+   * Returns an array of styles to be applied to the component.
+   * @returns {Array} An array containing the styles for the component.
+   */
   static get styles() {
     return [countdownTimerStyles];
   }
 
+  /**
+   * The properties of the countdown timer component.
+   * @returns {Object} The properties of the countdown timer component.
+   */
   static get properties() {
     return {
       time: { type: Number },
@@ -22,6 +30,10 @@ export class CountdownTimer extends i18nMixin(LitElement) {
     };
   }
 
+  /**
+   * Creates an instance of the CountdownTimer component.
+   * Initializes the interval, running state, translation function, and time.
+   */
   constructor() {
     super();
 
@@ -31,6 +43,12 @@ export class CountdownTimer extends i18nMixin(LitElement) {
     this.time = 0;
   }
 
+  /**
+   * Called when the element is added to the document's DOM.
+   * If the `time` property is greater than 0, it starts the countdown.
+   *
+   * @override
+   */
   connectedCallback() {
     super.connectedCallback();
 
@@ -39,8 +57,31 @@ export class CountdownTimer extends i18nMixin(LitElement) {
     }
   }
 
+  /**
+   * Called when the element is disconnected from the document's DOM.
+   * Clears the interval timer and sets it to null.
+   *
+   * @override
+   */
+  disconnectedCallback() {
+    super.disconnectedCallback();
+
+    clearInterval(this.interval);
+    this.interval = null;
+  }
+
+  /**
+   * Starts the countdown timer.
+   *
+   * This method initializes the countdown timer and ensures that it does not start multiple times concurrently.
+   * It decreases the time by one second intervals and updates the display accordingly.
+   * When the timer reaches zero, it stops the countdown and resets the running state.
+   *
+   * @returns {void}
+   */
   startCountdown() {
-    if (this.running) return; // Avoid starting the counter several times
+    // Avoid starting the counter several times
+    if (this.running) return;
 
     this.running = true;
 
@@ -56,11 +97,19 @@ export class CountdownTimer extends i18nMixin(LitElement) {
     }, ONE_SECOND); // Updates every second
   }
 
+  /**
+   * Stops the countdown timer by clearing the interval and setting the running flag to false.
+   */
   stopCountdown() {
     clearInterval(this.interval);
     this.running = false;
   }
 
+  /**
+   * Renders the countdown timer component.
+   *
+   * @returns {TemplateResult} The HTML template for the countdown timer component.
+   */
   render() {
     return html`
       <div>
